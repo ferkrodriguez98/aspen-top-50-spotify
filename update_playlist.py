63124b04e50cb517b5b16a70f7c6fb207ab52a67
ferkrodriguez98 <ferkrodriguez98@gmail.com>
2022-09-19 00:14:00 -0300

    updating files

--- update_playlist.py
+++ update_playlist.py
@@ -2,7 +2,7 @@ import functions
 
 # this should run only once per day
 
-tracks = functions.get_tracks_from_file()
+tracks = functions.get_lines_from_file("tracks.txt")
     
 ordered_tracks = functions.top_50(tracks)
 
